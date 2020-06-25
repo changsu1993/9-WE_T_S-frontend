0669c62637b613c274c60ec9860f36c2da513d8f
sonhyein <shl7683@gmail.com>
2020-06-25 15:25:34 +0900

    메인페이지 레이아웃 구현, Nav바 드롭다운 구현

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import "./Styles/common.scss";
+import "./Styles/reset.scss";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
