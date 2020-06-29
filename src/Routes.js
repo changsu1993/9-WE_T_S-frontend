5eeea6c4808b21867b905c90302223e6147fcdfc
changsu1993 <csdp0106@gmail.com>
2020-06-29 11:17:02 +0900

    뉴스레터 및 서치 구현

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Account from "./Pages/Account/Account";
+import Newsletter from "./Components/Newsletter/Newsletter";
 import Main from "./Pages/Main/Main";
 import Man from "./Pages/Products/Man";
 import Woman from "./Pages/Products/Woman";
 import Accessories from "./Pages/Products/Accessories";
 import Shoes from "./Pages/Products/Shoes";
 import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+import Search from "./Components/Search/Search";
 
 class Routes extends React.Component {
   render() {
@@ -15,6 +17,8 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/account" component={Account} />
+          <Route exact path="/newsletter" component={Newsletter} />
+          <Route exact path="/search" component={Search} />
           <Route exact path="/shopping/man" component={Man} />
           <Route exact path="/shopping/woman" component={Woman} />
           <Route exact path="/shopping/accessories" component={Accessories} />
